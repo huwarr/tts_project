scaling of pitch and energy + correct statistics for quantization

spec_to_wav/get_wav.py
@@ -38,14 +38,14 @@ def get_data(train_config):
     return data_list
 
 
-def run_full_synthesis(checkpoint_path='checkpoint.pth.tar', logger=None):
+def run_full_synthesis(pitch_min, pitch_max, energy_min, energy_max, checkpoint_path='checkpoint.pth.tar', logger=None):
     train_config = TrainConfig()
     WaveGlow = utils.get_WaveGlow()
     WaveGlow = WaveGlow.to(train_config.device)
 
     model_config = FastSpeechConfig()
     mel_config = MelSpectrogramConfig()
-    model = FastSpeech2(model_config, mel_config)
+    model = FastSpeech2(model_config, mel_config, pitch_min, pitch_max, energy_min, energy_max)
     model.load_state_dict(torch.load(checkpoint_path, map_location='cuda:0')['model'])
     model = model.eval()
     model = model.to(train_config.device)

train.py
@@ -31,7 +31,7 @@ train_config = TrainConfig()
 
 
 # define dataloader
-buffer = get_data_to_buffer(train_config, mel_config)
+buffer, pitch_min, pitch_max, energy_min, energy_max = get_data_to_buffer(train_config, mel_config)
 dataset = BufferDataset(buffer)
 
 training_loader = DataLoader(
@@ -44,7 +44,7 @@ training_loader = DataLoader(
 )
 
 # training essentials
-model = FastSpeech2(model_config, mel_config)
+model = FastSpeech2(model_config, mel_config, pitch_min, pitch_max, energy_min, energy_max)
 model = model.to(train_config.device)
 model.train()
 
@@ -142,7 +142,7 @@ for epoch in range(train_config.epochs):
                 )}, os.path.join(train_config.checkpoint_path, 'checkpoint_%d.pth.tar' % current_step))
                 print("save model at step %d ..." % current_step)
 
-                run_full_synthesis(checkpoint_path=os.path.join(train_config.checkpoint_path, 'checkpoint_%d.pth.tar' % current_step), logger=logger)
+                run_full_synthesis(pitch_min, pitch_max, energy_min, energy_max, checkpoint_path=os.path.join(train_config.checkpoint_path, 'checkpoint_%d.pth.tar' % current_step), logger=logger)
 
 
 # save checkpoint of the trained model
@@ -151,5 +151,6 @@ torch.save(
     'checkpoint.pth.tar'
 )
 
+run_full_synthesis(pitch_min, pitch_max, energy_min, energy_max, checkpoint_path='checkpoint.pth.tar', logger=logger)
 
 logger.finish_wandb_run()

tts/configs/all_configs.py
@@ -54,13 +54,13 @@ class TrainConfig:
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     batch_size = 64
-    epochs = 300
-    n_warm_up_step = 500
+    epochs = 250
+    n_warm_up_step = 300
 
     learning_rate = 1e-3
     weight_decay = 1e-6
     grad_clip_thresh = 1.0
-    decay_step = [12500, 25000, 50000]
+    decay_step = [6250, 12500, 25000]
 
     save_step = 2000
     log_step = 5

tts/model/fastspeech2.py
@@ -67,7 +67,7 @@ class PredictorBlock(nn.Module):
 
 
 class VarianceAdaptor(nn.Module):
-    def __init__(self, model_config):
+    def __init__(self, model_config, pitch_min, pitch_max, energy_min, energy_max):
         super().__init__()
 
         self.duration_predictor = PredictorBlock(model_config)
@@ -76,6 +76,11 @@ class VarianceAdaptor(nn.Module):
 
         self.pitch_embed = nn.Embedding(256, model_config.encoder_dim)
         self.energy_embed = nn.Embedding(256, model_config.encoder_dim)
+
+        self.pitch_min = pitch_min
+        self.pitch_max = pitch_max
+        self.energy_min = energy_min
+        self.energy_max = energy_max
     
     def LR(self, x, duration_predictor_output, mel_max_length=None):
         expand_max_len = torch.max(
@@ -109,12 +114,12 @@ class VarianceAdaptor(nn.Module):
             ).long().to(x.device)
 
         pitch = self.pitch_predictor(x) * pitch_alpha
-        buckets = torch.linspace(torch.log(pitch.min() + 1).item(), torch.log(pitch.max() + 1).item(), 256).to(x.device)
+        buckets = torch.linspace(torch.log(torch.tensor(self.pitch_min) + 1).item(), torch.log(torch.tensor(self.pitch_max) + 1).item(), 256).to(x.device)
         pitch_quantized = torch.bucketize(torch.log(pitch + 1), buckets[:-1]).to(x.device)
         
         energy = self.energy_predictor(x) * energy_alpha
-        buckets = torch.linspace(energy.min().item(), energy.max().item(), 256).to(x.device)
-        energy_quantized = torch.bucketize(torch.log(energy), buckets[:-1]).to(x.device)
+        buckets = torch.linspace(torch.tensor(self.energy_min), torch.tensor(self.energy_max), 256).to(x.device)
+        energy_quantized = torch.bucketize(energy, buckets[:-1]).to(x.device)
 
 
         out = self.pitch_embed(pitch_quantized) + self.energy_embed(energy_quantized) + x
@@ -122,13 +127,13 @@ class VarianceAdaptor(nn.Module):
 
 
 class FastSpeech2(nn.Module):
-    def __init__(self, model_config, mel_config):
+    def __init__(self, model_config, mel_config, pitch_min, pitch_max, energy_min, energy_max):
         super().__init__()
 
         self.encoder = Encoder(model_config)
         self.decoder = Decoder(model_config)
 
-        self.variance_adaptor = VarianceAdaptor(model_config)
+        self.variance_adaptor = VarianceAdaptor(model_config, pitch_min, pitch_max, energy_min, energy_max)
     
         self.mel_linear = nn.Linear(model_config.decoder_dim, mel_config.num_mels)
 

tts/utils/data_utils.py
@@ -7,6 +7,7 @@ import numpy as np
 import pyworld
 import torchaudio
 from torchaudio.transforms import Spectrogram, MelScale
+from sklearn.preprocessing import StandardScaler
 
 from text import text_to_sequence
 
@@ -26,6 +27,9 @@ def get_data_to_buffer(train_config, melspec_config):
 
     wav_to_spec = Spectrogram(n_fft=1024, hop_length=256, power=1)
     spec_to_mels = MelScale(n_mels=melspec_config.num_mels, n_stft=1024 // 2 + 1, sample_rate=22050, f_min=0., f_max=8000, norm='slaney', mel_scale='slaney')
+
+    pitch_scaler = StandardScaler()
+    energy_scaler = StandardScaler()
 
     start = time.perf_counter()
     for i in tqdm(range(len(text))):
@@ -51,6 +55,9 @@ def get_data_to_buffer(train_config, melspec_config):
         character = torch.from_numpy(character)
         duration = torch.from_numpy(duration)
 
+        pitch_scaler.partial_fit(pitch.reshape(-1, 1))
+        energy_scaler.partial_fit(energy.numpy().reshape(-1, 1))
+
         buffer.append(
             {
                 "text": character, 
@@ -60,8 +67,26 @@ def get_data_to_buffer(train_config, melspec_config):
                 "mel_target": mel_spec
             }
         )
+    
+    pitch_min = float('+inf')
+    pitch_max = float('-inf')
+    energy_min = float('+inf')
+    energy_max = float('-inf')
+    buffer_new = []
+    for item in buffer:
+        pitch = item['pitch']
+        pitch_normalized = pitch_scaler.transform(pitch.numpy().reshape(-1, 1)).reshape(-1)
+        energy = item['energy']
+        energy_normalized = energy_scaler.transform(energy.numpy().reshape(-1, 1)).reshape(-1)
+        item['pitch'] = torch.from_numpy(pitch_normalized)
+        item['energy'] = torch.from_numpy(energy_normalized)
+        buffer_new.append(item)
+        pitch_min = min(pitch_min, pitch_normalized.min())
+        pitch_max = max(pitch_max, pitch_normalized.max())
+        energy_min = min(energy_min, energy_normalized.min())
+        energy_max = max(energy_max, energy_normalized.max())
 
     end = time.perf_counter()
     print("cost {:.2f}s to load all data into buffer.".format(end-start))
 
-    return buffer
+    return buffer_new, pitch_min, pitch_max, energy_min, energy_max